2e929526c57b3415c4c9433a2cc66ca778fc8146
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2018-04-11 14:44:03 -0700

    reading input from files

--- source/hw1.cpp
+++ source/hw1.cpp
@@ -3,9 +3,36 @@
 //
 
 #include "lambda_parser.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 int main()
 {
+	string inputFilename;
+	cout << "enter input filename: " << endl;
+	cin >> inputFilename;
+
+	string outputFilename;
+	cout << "enter output filename: " << endl;
+	cin >> outputFilename;
+
+	ifstream t(inputFilename);
+	string str((std::istreambuf_iterator<char>(t)),
+					std::istreambuf_iterator<char>());
+
+
+	LambdaExpr* expr = LambdaExpr::parse(str);
+	while (expr->isReducable())
+	{
+		expr = expr->reduce();
+	}
+
+	ofstream output_f(outputFilename);
+	output_f << *expr;
+
 	return 0;
 }
 
