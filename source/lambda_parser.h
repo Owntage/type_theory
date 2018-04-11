3447fbe6e2b2aa2560e01d901a20c1433b229fc9
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2018-04-10 22:44:02 -0700

    printing to stream implemented

--- source/lambda_parser.h
+++ source/lambda_parser.h
@@ -6,6 +6,7 @@
 #define MATH_LOGIC_HW_LAMBDA_PARSER_H
 
 #include <string>
+#include <sstream>
 #include "lambda_parser.h"
 
 struct LambdaExpr
@@ -19,31 +20,26 @@ struct LambdaExpr
 	static LambdaExpr* createVar(std::string var);
 
 	static LambdaExpr* parse(const std::string& input);
+	
+	void substitute(std::string var, LambdaExpr* expr);
+	LambdaExpr* reduce();
+	std::string toString();
+
+	bool isAbstraction() { return _isAbstraction; }
+	bool isApplication() { return _isApplication; }
+	bool isVariable() { return !isAbstraction() && !isApplication(); }
 
 	template<typename Stream>
 	static Stream& operator<<(Stream& stream, LambdaExpr expr)
 	{
-
-	}
-
-	void substitute(std::string var, LambdaExpr* expr);
-	LambdaExpr* reduce();
-
-
-
-	bool isAbstraction()
-	{
-		return _isAbstraction;
-	}
-
-	bool isApplication()
-	{
-		return _isApplication;
-	}
-
-	bool isVariable()
-	{
-		return !isAbstraction() && !isApplication();
+		if (expr.isAbstraction())
+		{
+			stream << "\\" << expr.value << ".(" << *expr.left << ")";
+		}
+		if (expr.isApplication())
+		{
+			stream << "(" << *expr.left << ")(" << *expr.right << ")";
+		}
 	}
 
 private:
